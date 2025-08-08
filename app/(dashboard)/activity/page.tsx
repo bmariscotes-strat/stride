e0de9c8f6c49f37a71931e3a44ec2263611d944b
Biella Mariscotes <i_biellamae.mariscotes@stratpoint.com>
2025-08-09 00:06:39 +0800

    fix(activity): enforced dynamic

--- app/(dashboard)/activity/page.tsx
+++ app/(dashboard)/activity/page.tsx
@@ -3,6 +3,9 @@ import ActivityLogsClient from "./Activity.client";
 import { ACTIVITY_LOGS_LIMIT as LIMIT } from "@/lib/constants/limits";
 import { getRequiredUserId } from "@/lib/utils/get-current-user";
 
+// Add this line to force dynamic rendering
+export const dynamic = "force-dynamic";
+
 export default async function ActivityLogsPage() {
   const userId = await getRequiredUserId();
 
